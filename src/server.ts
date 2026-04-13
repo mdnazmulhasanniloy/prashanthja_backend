86ae51888a105e41c507f28f8695395d596ded60
MD Nazmul Hasan <105441794+mdnazmulhasanniloy@users.noreply.github.com>
2026-04-13 12:44:23 +0600

    image get error resolved

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,6 @@ import './app/job/croneJob';
 
 let server: Server;
 const socketServer = createServer(app);
-const socketIp = '103.186.20.117';
 
 async function main() {
   try {
@@ -28,7 +27,7 @@ async function main() {
     io.listen(Number(config.socket_port));
     console.log(
       colors.yellow.bold(
-        `⚡Socket.io running on  http://${socketIp}:${config.socket_port}`,
+        `⚡Socket.io running on  http://${config?.ip}:${config.socket_port}`,
       ),
     );
   } catch (err) {
